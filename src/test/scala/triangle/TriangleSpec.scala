4b65108041c20a30299031a2ca03f745082051cc
tzimisce012 <daniel.ochoa@47deg.com>
2020-02-29 14:30:53 +0100

    Improve tests

--- src/test/scala/triangle/TriangleSpec.scala
+++ src/test/scala/triangle/TriangleSpec.scala
@@ -1,23 +1,42 @@
 package triangle
 
+import org.scalatest.concurrent.TimeLimits
+import org.scalatest.time.{Millis, Span}
 import org.scalatest.{FlatSpec, Matchers}
 
-class TriangleSpec extends FlatSpec with Matchers {
+class TriangleSpec extends FlatSpec with Matchers with TimeLimits {
 
   behavior of "A Triangle"
 
   it should "find its minimum path in a trivial case" in {
-    Triangle.findMinimumPath(Array(Array(4))) shouldEqual Array(4)
+    val (value, path) = Triangle.findMinimumPath(Array(Array(4)))
+    value shouldBe 4
+    path sameElements Array(4) shouldBe true
   }
 
   it should "find its minimum path" in {
+
     val triangle = Array(
       Array(7),
       Array(6,3),
       Array(3,8,5),
       Array(11, 2, 10, 9)
     )
-    Triangle.findMinimumPath(triangle) shouldEqual Array(7, 6, 3, 2)
+
+    val (value, path) = Triangle.findMinimumPath(triangle)
+    value shouldBe 18
+    path sameElements Array(7, 6, 3, 2) shouldBe true
+  }
+
+  it must "work for a big triangle" in {
+    val r = scala.util.Random
+    val triangle = (for (j <- 1 to 500) yield {
+      (for (i <- 1 to j) yield r.nextInt(i)).toArray
+    }).toArray
+
+    failAfter(Span(100, Millis)) {
+      Triangle.findMinimumPath(triangle)
+    }
   }
 
 }
